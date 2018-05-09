8defe3d9cca9c082bbe781aa79b9de0ab04adb4e
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-05-09 16:38:46 -0700

    added designs for each post, given options, some design decisions needs to be make

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import {Card, CardMedia, CardTitle} from 'material-ui/Card'
+import {Card, CardMedia, CardTitle, CardText, CardHeader, CardActions} from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
 const Post = (props) => 
     <Card>
+        <CardHeader
+      title="URL Avatar"
+      subtitle="Subtitle"
+      showExpandableButton={true}
+        />
         <CardMedia>
             <img src={props.media} alt="" />
         </CardMedia>
+        <CardTitle title="Card title" showExpandableButton={true}/>
+        <CardText expandable={true}>
+            Yo here is some text 
+        </CardText>
+        <CardActions>
+            <FlatButton label="Give" />
+        </CardActions>
     </Card>     
 
 
